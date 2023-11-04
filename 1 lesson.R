e94612d45b3a2c20f583dc3939b4b2d410324121
Irina <Smolenskaya2002@yandex.ru>
2023-11-04 22:35:07 +0300

    Add files via upload

---
+++ 1 lesson.R
@@ -0,0 +1,18 @@
+10 : 30
+mean(10:30)
+
+#Step 2: Variable assiment 
+my_var1  <- 42 
+my_var2  <- 35.25  
+my_var1 + 100 
+my_var1 + my_var2 - 12 
+my_var3  <- my_var1^2 + my_var2^2  
+
+#Step 3: Logical opperartions 
+my_var3 > 200 
+my_var3 > 3009 
+my_var1 == my_var2 
+my_var1 != my_var2 
+my_var3 >= 200 
+my_var3 <= 200  
+my_new_var  <- my_var1 == my_var2
